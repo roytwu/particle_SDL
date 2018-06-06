6165a276acc0e3387443c8ee8d2ea8810a4d86c1
roy <tsehuaiwu@gmail.com>
2018-06-05 20:04:19 -0700

    updating installation tutorial -- install xorg-dev

--- main.cpp
+++ main.cpp
@@ -10,10 +10,12 @@ int main(){
 
 	if(SDL_Init(SDL_INIT_VIDEO) < 0) {
 		cout << "SDL Init failed." << endl;
+		cout << SDL_Init(SDL_INIT_VIDEO) << endl; 
+		cout << SDL_GetError() << endl;
 		return 1;
 	}
 
-	cout << endl << "SDL Init succeedeed!" << endl;
+	cout << endl << "SDL Init succeeded!" << endl;
 	
 	SDL_Quit();
 
